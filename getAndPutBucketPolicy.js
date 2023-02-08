9de15c8e4c9b0396d7bdf376d5dcb8a49072681d
Vaibhav <vbhapkar@uptycs.com>
2023-02-09 00:31:34 +0530

    code cleaning

--- getAndPutBucketPolicy.js
+++ getAndPutBucketPolicy.js
@@ -1,13 +1,25 @@
 "use strict";
 const {
-  S3Client,
-  PutBucketPolicyCommand,
-  GetBucketPolicyCommand,
+    S3Client,
+    PutBucketPolicyCommand,
+    GetBucketPolicyCommand,
 } = require("@aws-sdk/client-s3");
 const {
-  sendToUptycsRemediationFeedback,
-  ApplicationError,
+    sendToUptycsRemediationFeedback,
+    ApplicationError,
 } = require("../utils");
+
+const adderPolicy = {
+    Effect: "Deny",
+    Principal: "*",
+    Action: "s3:GetObject",
+    Resource: "arn:aws:s3:::<bucket_name>/*",
+    Condition: {
+        Bool: {
+            "aws:SecureTransport": "false",
+        },
+    },
+};
 
 /**
  *  Block S3 from Public access
@@ -17,50 +29,56 @@ const {
  * @returns
  */
 module.exports.handler = async (event, context, callback) => {
-  event = event || {};
+    event = event || {};
 
-  console.log(
-    " Block S3 Public Access  - Received event:",
-    JSON.stringify(event, null, 2)
-  );
-  try {
-    if (!event.region) {
-      throw new ApplicationError("Invalid event, no region provided");
+    console.log(
+        " Block S3 Public Access  - Received event:",
+        JSON.stringify(event, null, 2)
+    );
+    try {
+        if (!event.region) {
+            throw new ApplicationError("Invalid event, no region provided");
+        }
+        event.lambdaExecutorRecievedTime = new Date();
+
+        const client = new S3Client({ region: event.region });
+
+        const bucketName = {
+            Bucket: event.bucketName,
+        };
+
+        // GET BUCKET POLICY
+        const getCommand = new GetBucketPolicyCommand(bucketName);
+        const policy = await client.send(getCommand);
+
+        if (!policy) {
+            throw new ApplicationError("Invalid bucket name or policy not found");
+        }
+        event.lambdaExecutorRecievedTime = new Date();
+
+        // response will be policy in string format
+        policy = JSON.parse(policy);
+
+        //adderPolicy is policy which will get added to the existing bucket
+        adderPolicy.Resource = adderPolicy.Resource.replace(
+            "<bucket_name>",
+            event.bucketName
+        );
+        policy.Statement.push(adderPolicy);
+
+        policy = JSON.stringify(policy);
+
+        // PUT BUCKET POLICY
+        const putCommand = new PutBucketPolicyCommand(policy);
+        const putResponse = await client.send(putCommand);
+        event.response = putResponse;
+
+        console.log(putResponse);
+    } catch (err) {
+        event.error = err;
+        console.error("Error : ", err.message);
+    } finally {
+        event.lambdaFeedbackDispachedTime = new Date();
+        await sendToUptycsRemediationFeedback(event);
     }
-    event.lambdaExecutorRecievedTime = new Date();
-
-    const client = new S3Client({ region: event.region });
-
-    const input = ""; //bucket name;
-
-    // GET BUCKET POLICY
-    const command = new GetBucketPolicyCommand(input);
-    const policy = await client.send(command);
-    event.response = policy;
-    
-    if (!event.response) {
-        throw new ApplicationError("Invalid bucket name");
-    }
-    
-    // response will be policy in string format
-    policy = JSON.parse(policy);
-
-    //adderPolicy is policy which will get added to the existing bucket
-    adderPolicy.Resource = adderPolicy.Resource.replace("<bucket_name>", input); 
-    policy.Statement.push(adderPolicy);
-    
-
-    policy = JSON.stringify(policy);
-    
-    // PUT BUCKET POLICY
-    const putCommand = new PutBucketPolicyCommand(policy);
-    const putResponse = await client.send(putCommand);
-    console.log(putResponse);
-  } catch (err) {
-    event.error = err;
-    console.error("Error : ", err.message);
-  } finally {
-    event.lambdaFeedbackDispachedTime = new Date();
-    await sendToUptycsRemediationFeedback(event);
-  }
 };
